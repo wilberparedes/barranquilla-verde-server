2e557e72fb78a96c3064d76a5738e593faf0cd22
Wilber Paredes <wilberparedes.intellinext@gmail.com>
2021-06-04 15:21:16 -0500

    add send email

--- developer/code.php
+++ developer/code.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
-// date_default_timezone_set('Pacific/Honolulu');
+// date_default_timezone_set('America/Bogota');
+// date_default_timezone_set('Europe/Amsterdam');
 header('content-type: application/json; charset=utf-8');
 
 require_once 'PDOConn.php';
@@ -20,12 +21,15 @@ $mail->Port = 587;
 // $mail->Username = "wilberparedes@gmail.com";
 // $mail->Password = "1461217arca#";
 // $mail->SetFrom('wilberparedes@gmail.com', utf8_decode('Corporación Universitaria Americana'));
+$tz_object = new DateTimeZone('America/Bogota');
+$objDateTime = new DateTime('NOW');
+$objDateTime->setTimezone($tz_object);
 
 $mail->Username = "wilberparedesg@gmail.com";
 $mail->Password = "1461217Arca!";
 $mail->SetFrom('wilberparedesg@gmail.com', utf8_decode('Barranquilla verde'));
 
-$mail->Subject = utf8_decode("Nuevo reporte | Barranquilla verde");
+$mail->Subject = utf8_decode("Nuevo reporte | Barranquilla verde | ".$objDateTime->format('d/m/Y h:i a'));
 $mail->AltBody = "";
 
 
@@ -140,10 +144,9 @@ switch ($case) {
         $urlFoto='assets/'.$_GET['nombphoto'].".".$extension[1];
 
         if (move_uploaded_file($_FILES['img-perfil']['tmp_name'],$url)) {
-          // $sql="UPDATE usuarios SET foto='". $urlFoto."' WHERE codigo_usu='".$_SESSION['CA_codigo_usuCA']."'"; 
-          // query($sql);
-          // $_SESSION['CA_foto']=$urlFoto;
           $json = json_encode(array("success" => true, "mensaje"=>"Foto subida exitosamente. ".$urlFoto)); 
+
+          
         }
 
       }else{
@@ -155,6 +158,10 @@ switch ($case) {
 
     case 'saveReport':
       if($_FILES['img-perfil']['tmp_name']!=""){
+
+        $sqlParque = "SELECT * FROM parques WHERE id_prq = :idparque";
+        $rowParque = row($sqlParque, array(':idparque' => $idparque));
+
         $file=$_FILES["img-perfil"]['name'];
         $extension= explode(".",$file) ;
         $newnamefile = $_GET['nombphoto'].".".$extension[1];
@@ -163,17 +170,184 @@ switch ($case) {
 
         if (move_uploaded_file($_FILES['img-perfil']['tmp_name'],$url)) {
           
-          $insert = "INSERT INTO reportes (id_usuario_fk, id_parque_fk, tipo, comentario, imagen) VALUES (:iduser, :idparque, :zonanovedad, :comentario, :imagen) RETURNING id_rp";
+          $insert = "INSERT INTO reportes (id_usuario_fk, id_parque_fk, tipo, comentario, imagen, fecha) VALUES (:iduser, :idparque, :zonanovedad, :comentario, :imagen, :fecha) RETURNING id_rp";
           $paramsInsert = array(
                                 ':iduser' => $iduser,
                                 ':idparque' => $idparque, 
                                 ':zonanovedad' => $zonanovedad,
                                 ':comentario' => $comentario,
                                 ':imagen' => $newnamefile,
+                                ':fecha' => $objDateTime->format('Y-m-d H:i:s'),
                               );
           $datarow = DataRow($insert,$paramsInsert);
           if($datarow != -1){
-            $json = json_encode(array("success" => true, "idrp" => $datarow["id_rp"]));
+            
+
+            $html = "<!DOCTYPE html>";
+            $html .= "<html>";
+            $html .= "<head>";
+            $html .= "<title>REPORTE DE NOVEDAD | BARRANQUILLA VERDE</title>";
+            $html .= "<meta charset='UTF-8'>";
+            $html .= "<style> 
+                body{
+                    text-align:center;
+                }
+              /*button*/
+                .button{
+                  display: inline-block;
+                  padding:0 10px;
+                  color: #4c4c4c;
+                  height: 40px;
+                  text-align: center;
+                  line-height: 35px;
+                  white-space: nowrap;
+                    cursor: pointer;
+                  box-sizing: border-box; 
+                  border: 1px solid #CCCCCC;
+                  margin-bottom:10px;
+                  border-radius:3px;
+                  font-weight: bold;
+                  outline: 0;
+                  text-decoration: none;
+                }
+                .primary-button{
+                  color: #fff !important;
+                  background: #004e93!important;
+                  border: 1px solid #004e93;
+                }
+            </style>";
+            $html .="</head>";
+            $html .="<body>";
+            $html .= '<table style="width:100%;max-width:600px" width="100%" cellspacing="0" cellpadding="0" border="0" align="center">';
+            $html .= '<tbody>';
+            $html .= '<tr>';
+            $html .= '<td role="modules-container" style="padding:0px 0px 0px 0px;color:#000000;text-align:left" width="100%" bgcolor="#ffffff" align="left">';
+            $html .= '<table style="table-layout:fixed" width="100%" cellspacing="0" cellpadding="0" border="0">';
+            $html .= '<tbody>';
+            $html .= '<tr>';
+            $html .= '<td style="font-size:6px;line-height:10px;padding:0px 0px 0px 0px" valign="top" align="center">';
+            $html .= '<img style="display:block;max-width:100%!important;width:100%;height:auto!important" src="../assets/header-email.png" width="600" border="0">';
+            $html .= '</td>';
+            $html .= '</tr>';
+            $html .= '</tbody>';
+            $html .= '</table>';
+      
+            $html .= '<table style="table-layout:fixed" width="100%" cellspacing="0" cellpadding="0" border="0">';
+            $html .= '<tbody>';
+            $html .= '<tr>';
+            $html .= '<td style="padding:0px 0px 20px 0px">';
+            $html .= '</td>';
+            $html .= '</tr>';
+            $html .= '</tbody>';
+            $html .= '</table>';
+
+            $html .= '<table style="table-layout:fixed" width="100%" cellspacing="0" cellpadding="0" border="0">';
+            $html  .= '<tbody>';
+            $html .= '<tr>';
+            $html .= '<td style="padding:5px 0px 0px 0px;line-height:22px;text-align:inherit" valign="top" height="100%">';
+            $html .= '<div>';
+            $html .= '<span style="font-size:18px";font-family:arial,helvetica,sans-serif">';
+            $html .= '<strong>'.$rowParque["name_prq"].'</strong>';
+            $html .='</span></div>';
+            $html  .='</td>';
+            $html .= '</tr>';
+
+            $html .= '<tr>';
+            $html .= '<td style="padding:5px 0px 5px 0px;line-height:22px;text-align:inherit" valign="top" height="100%">';
+            $html .= '<div>';
+            $html .= '<span style="font-size:14px";font-family:arial,helvetica,sans-serif">';
+            $html .= '<strong>Dirección: </strong> '.$rowParque["address"];
+            $html .='</span></div>';
+            $html  .='</td>';
+            $html .= '</tr>';
+
+            $html .= '<tr>';
+            $html .= '<td style="padding:0px 0px 5px 0px;line-height:22px;text-align:inherit" valign="top" height="100%">';
+            $html .= '<div>';
+            $html .= '<span style="font-size:14px";font-family:arial,helvetica,sans-serif">';
+            $html .= '<strong>Barrio: </strong> '.$rowParque["neighborhood"];
+            $html .='</span></div>';
+            $html  .='</td>';
+            $html .= '</tr>';
+
+            $html .= '<tr>';
+            $html .= '<td style="padding:10px 0px 5px 0px;line-height:22px;text-align:inherit" valign="top" height="100%">';
+            $html .= '<div>';
+            $html .= '<span style="font-size:16px";font-family:arial,helvetica,sans-serif">';
+            $html .= '<strong>Detalle de reporte: </strong>';
+            $html .='</span></div>';
+            $html  .='</td>';
+            $html .= '</tr>';
+
+            $html .= '<tr>';
+            $html .= '<td style="padding:0px 0px 5px 0px;line-height:22px;text-align:inherit" valign="top" height="100%">';
+            $html .= '<div>';
+            $html .= '<span style="font-size:14px";font-family:arial,helvetica,sans-serif">';
+            $html .= '<strong>Tipo: </strong>';
+            if($zonanovedad == 'zonasverdes'){
+              $html .= 'Novedad Zonas verdes';
+            }
+            else if($zonanovedad == 'gimnasio'){
+              $html .= 'Novedad Gimnasio biosaludable';
+            }
+            else{
+              $html .= 'Novedad Parques';
+            }
+
+            $html .='</span></div>';
+            $html  .='</td>';
+            $html .= '</tr>';
+            $html .= '<tr>';
+            $html .= '<td style="padding:0px 0px 5px 0px;line-height:22px;text-align:inherit" valign="top" height="100%">';
+            $html .= '<div>';
+            $html .= '<span style="font-size:14px";font-family:arial,helvetica,sans-serif">';
+            $html .= '<strong>Comentarios: </strong> <br />';
+            $html .= $comentario;
+            $html .='</span></div>';
+            $html  .='</td>';
+            $html .= '</tr>';
+
+            $html .= '<tr>';
+            $html .= '<td style="padding:0px 0px 5px 0px;line-height:22px;text-align:inherit" valign="top" height="100%">';
+            $html .= '<div>';
+            $html .= '<span style="font-size:14px";font-family:arial,helvetica,sans-serif">';
+            $html .= '<strong>Evidencia: </strong>';
+            $html .='</span></div>';
+            $html  .='</td>';
+            $html .= '</tr>';
+
+            $html .= '<tr>';
+            $html .= '<td style="padding:0px 0px 0px 0px;line-height:22px;text-align:inherit" valign="top" height="100%">';
+            $html .= '<img style="display:block;max-width:70%!important;width:100%;height:auto!important" src="'.$url.'">';
+            $html  .='</td>';
+            $html .= '</tr>';
+
+            $html .= '</tbody>';
+            $html .= '</table>';
+      
+            $html .="</body>";
+            $html .="</html>";
+            $mail->MsgHTML($html);
+            $mail->AddAddress($email,"");
+            $mail->AddBCC("wilberparedes@gmail.com","");
+
+            $mail->IsHTML(true);
+            $mail->smtpConnect(
+            array(
+              "ssl" => array(
+                  "verify_peer" => false,
+                  "verify_peer_name" => false,
+                  "allow_self_signed" => true
+              )
+            )
+            );
+
+            if ($mail->Send()) {
+              $json = json_encode(array("success" => true, "idrp" => $datarow["id_rp"]));
+            }else{
+              $json=json_encode(array("success"=>true,  "idrp" => $datarow["id_rp"],  "message"=>$mail->ErrorInfo));
+            }
+
           }else{
             $json = json_encode(array("success" => false,"message" => "Error registrar novedad, por favor, intente de nuevo más tarde"));
           }
@@ -218,6 +392,11 @@ switch ($case) {
           $json = json_encode(array("success"=>false,"mensaje" => "La contraseña actual ingresada no es correcta"));
         }
       }
+    break;
+
+
+    case 'Prueba':
+      $json = json_encode(array("success"=>true, "datetime" => $objDateTime->format('Y-m-d H:i:s') ));
     break;
 
     case 'LoadParques':
